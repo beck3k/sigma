0a793085de35f1b3be176ce8881e45f39e0fb87a
Shivam Garg <shivamg2604@gmail.com>
2021-10-05 00:50:47 -0400

    update

--- deso frontend/src/app/stream-view/stream-view.component.ts
+++ deso frontend/src/app/stream-view/stream-view.component.ts
@@ -1,47 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, ElementRef, OnDestroy, ViewChild} from '@angular/core';
+import videojs from 'video.js';
 import { AppRoutingModule } from "../app-routing.module";
 import { GlobalVarsService } from "../global-vars.service";
 import { Router } from "@angular/router";
 import { SearchBarComponent } from '../search-bar/search-bar.component'
 import { HttpClient } from '@angular/common/http';
-import Clappr from '@clappr/core'
-import * as hlsjs from 'p2p-media-loader-hlsjs';
+declare var require: any;
+require('videojs-contrib-quality-levels');
+require('videojs-hls-quality-selector');
 
 @Component({
   selector: 'app-stream-view',
   templateUrl: './stream-view.component.html',
   styleUrls: ['./stream-view.component.scss']
 })
-export class StreamViewComponent implements OnInit {
+export class StreamViewComponent implements OnInit, AfterViewInit, OnDestroy {
   streamers
   constructor(public globalVars: GlobalVarsService, private router: Router, private http: HttpClient) { }
 
+  public player: videojs.Player;
+
+
   ngOnInit(): void {
     this.getStreamers();
-    if (hlsjs.Engine.isSupported()) {
-      var engine = new hlsjs.Engine();
-      var loader = engine.createLoaderClass();
-    } else {
-      // var loader = XHRLoader;
-      console.log('fuck');
-    }
-    var engine = new hlsjs.Engine();
-    var player = new Clappr.Player({
-      parentId: "#video",
-      source: "http://149.159.16.161:8082/live/615bba8d7db93bbee3d42621/index.m3u8",
-      width:"100%",
-      height:"100%",
-      playback: {
-        hlsjsConfig: {
-          liveSyncDurationCount: 7,
-          loader: loader
-        }
-      }
-    });
-    if (hlsjs.Engine.isSupported()) hlsjs.initClapprPlayer(player);
-    player.play(true);
-    console.log(player);
-    console.log(hlsjs.Engine.isSupported())
   }
 
   createStream(){
@@ -52,6 +34,29 @@ export class StreamViewComponent implements OnInit {
 
   getStreamers() {
     this.http.get("http://149.159.16.161:3123/streams").subscribe((data)=>this.streamers = data)
+  }
+
+  ngAfterViewInit() {
+    const options = {
+      'sources': [{
+        'src': "http://149.159.16.161:8082/live/615bba8d7db93bbee3d42621/index.m3u8",
+        'type': 'application/x-mpegURL'
+      }
+      ],
+      // 'poster' : this.urlPoster
+    };
+    this.player = videojs('my-video', options, function onPlayerReady() {
+      console.log('Player ready');
+      var myPlayer = this, id = myPlayer.id();
+      myPlayer.hlsQualitySelector();
+    });
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.player != null) {
+      this.player.dispose();
+    }
   }
 
 
