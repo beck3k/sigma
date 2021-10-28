64353ae8a549675de6fc02ca1aee725913d67b6b
Shivam Garg <shivamg2604@gmail.com>
2021-10-28 09:39:29 -0400

    significants improvements on frontend

---
+++ .history/api/src/app_20211027193106.ts
@@ -0,0 +1,368 @@
+import express from 'express';
+import { CategoryModel, StreamModel, Stream, Category, ViewerModel } from 'db';
+import CryptoJS from 'crypto-js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import { Strategy as AuthStrategy } from 'passport-custom';
+import passport from 'passport';
+import jsonwebtoken from 'jsonwebtoken';
+import { ec as EC } from 'elliptic';
+import { default as KeyEncoder } from 'key-encoder';
+import bs58check from 'bs58check';
+
+const ec = new EC("secp256k1");
+
+const app = express()
+const port = 3123;
+
+import SocketBridge from './bridge';
+
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use(passport.initialize());
+
+passport.use('deso', new AuthStrategy((req, done) => {
+  var token = null;
+  var publicKey = null;
+  if(req && req.route.methods.post && req.body.JWT) {
+    token = req.body.JWT;
+    publicKey = req.body.PublicKeyBase58Check;
+  }else if(req && req.route.methods.get) {
+    token = req.headers['authorization'];
+    publicKey = req.headers['publickeybase58check'];
+  } else {
+    done(null, false);
+  }
+
+  try {
+    const decodedKey = bs58check.decode(publicKey);
+    const decodedKeyArray = [...decodedKey];
+    const rawPK = decodedKeyArray.slice(3);
+    const hexPK = ec.keyFromPublic(rawPK).getPublic().encode('hex', true);
+
+    const keyEncoder = new KeyEncoder("secp256k1");
+    const encodedPK = keyEncoder.encodePublic(hexPK, "raw", "pem");
+
+    const result = jsonwebtoken.verify(token, encodedPK, {
+      algorithms: ["ES256"]
+    });
+    done(null, result);
+  } catch(error) {
+    done(null, false);
+  }
+  
+}));
+
+app.get('/', (req, res) => {
+  res.send('API Alive');
+});
+
+function getKey(){
+  var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  var name = "";
+  var charactersLength = characters.length;
+  for ( var i = 0; i < 8; i++ ) {
+    name += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  var key = CryptoJS.SHA256("live/"+name).toString();
+  return key;
+}
+
+// Reset stream key
+app.post('/stream', passport.authenticate('deso', { session: false }), async (req, res) => {
+  var key = getKey();
+  var publicKey = req.body.PublicKeyBase58Check;
+
+  var stream = await StreamModel.findOneAndUpdate({
+    publicKey
+  }, {
+    key,
+    viewerCount: 0
+  }, {
+    new: true,
+    upsert: true
+  });
+  stream.save();
+  res.send({
+    _id: stream._id,
+    key,
+    streamKey: `${stream._id}?pwd=${key}`
+  });
+});
+
+// Get private stream data
+app.get('/private/stream', passport.authenticate('deso', { session: false }), async (req, res) => {
+  const publicKey: any = req.headers['publickeybase58check'];
+  var stream = await StreamModel.findOne({
+    publicKey
+  });
+
+  if(!stream) {
+    var key = getKey();
+    stream = await StreamModel.create({
+      publicKey,
+      key: key
+    });
+    stream.save();
+  }
+
+  res.json({
+    stream: {
+      ...stream,
+      streamKey: `${stream._id}?pwd=${stream.key}`
+    }
+  });
+});
+
+app.get('/streams', async (req, res) => {
+  const streams = await StreamModel.find({}, '-key');
+
+  res.send({
+    streams
+  });
+});
+
+app.get('/streams/live', async (req, res) => {
+  const streams = await StreamModel.find({
+    isLive: true
+  }, '-key');
+
+  res.send({
+    streams
+  });
+});
+
+app.get('/streams/live/one', async (req, res) => {
+  const stream = await StreamModel.findOne({
+    isLive: true
+  }, '-key');
+
+  res.send({
+    stream
+  });
+});
+
+app.get('/stream/:publicKey/info', async (req, res) => {
+  const stream = await StreamModel.findOne({
+    publicKey: req.params.publicKey
+  }, '-key');
+
+  res.json({
+    stream: {
+      title: stream.title,
+      description: stream.description,
+      category: stream.category,
+      isLive: stream.isLive,
+      viewerCount: stream.viewerCount
+    }
+  });
+});
+
+// Update Stream Info
+app.post('/stream/info', passport.authenticate('deso', { session: false }), async (req, res) => {
+  var publicKey = req.body.PublicKeyBase58Check;
+  const stream = await StreamModel.findOneAndUpdate({
+    publicKey
+  }, {
+    title: req.body.title,
+    description: req.body.description,
+    category: req.body.category,
+  });
+
+  console.log(req.body);
+
+  const fuckingOld = await CategoryModel.updateOne({
+    streams: stream._id,
+  }, {
+    $pullAll: {
+      streams: [stream._id]
+    }
+  });
+
+  const newShit = await CategoryModel.updateOne({
+    _id: req.body.category,
+    streams: {
+      $ne: stream._id
+    }
+  }, {
+    $push: {
+      streams: stream._id
+    }
+  });
+
+  res.json({stream});
+});
+
+app.get('/stream/:pubkey', async (req, res) => {
+  const stream = await StreamModel.findOne({
+    publicKey: req.params.pubkey
+  }, '-key');
+
+  if(stream && stream.category) {
+    await stream.populate('category');
+  }
+
+  res.send({
+    stream
+  });
+});
+
+// PublicKey is of the streamer
+app.post('/follow/:publicKey', passport.authenticate('deso', { session: false }), async (req, res) => {
+  var publicKey = req.body.PublicKeyBase58Check;
+  // Check that viewer exists
+  await ViewerModel.findOneAndUpdate({
+    publicKey
+  }, {}, {
+    upsert: true
+  });
+  // Check if following already, and push
+  const followers = await ViewerModel.updateOne(
+    {
+      publicKey,
+      following: {
+        $ne: req.params.publicKey
+      }
+    },
+    {
+      $push: {
+        following: req.params.publicKey
+      },
+    }
+  );
+
+  res.json({
+    followers
+  })
+});
+
+// Public key is of the viewer, will not be required after Auth implemented
+app.get('/following', async (req, res) => {
+  const publicKey: any = req.headers['publickeybase58check'];
+  const following = await ViewerModel.findOne({
+    publicKey
+  });
+
+  res.json({
+    following: (following) ? following.following : []
+  });
+});
+
+app.post('/unfollow/:pubicKey', passport.authenticate('deso', { session: false }), async (req, res) => {
+  var publicKey = req.body.PublicKeyBase58Check;
+  const deleted = await ViewerModel.updateOne({
+    publicKey
+  },
+  {
+    $pullAll: {
+      following: [req.params.pubicKey]
+    }
+  });
+
+  res.send({
+    deleted
+  })
+});
+
+app.get('/categories', async (req, res) => {
+  const categories = await CategoryModel.find();
+
+  res.send({
+    categories
+  });
+});
+
+function isStream(obj: Stream | any ): obj is Stream {
+  return (obj && obj.key);
+}
+
+app.get('/category/:category', async (req, res) => {
+    try {
+      const fuckYou = await CategoryModel.findById(req.params.category);
+      if(fuckYou) {
+        await CategoryModel.findById(req.params.category).populate('streams').exec((err, category) => {
+          if(category.streams) {
+            var streams = category.streams.map((stream) => {
+              if(isStream(stream)) {
+                if(stream.isLive) {
+                  return stream;
+                }
+              }
+            });
+            res.json({
+              streams: streams
+            });
+          } else {
+            res.json({
+              streams: null,
+              fuck: true
+            })
+          }
+        });
+      } else {
+        res.send('Fuck off');
+      }
+    } catch(e) {
+      res.json({
+        category: null,
+        categoryNotFound: true
+      });
+    }
+   
+
+  // res.json({
+  //   category: category.streams.map((stream) => {
+  //     console.log(stream.isLive);
+  //     // if(stream.isLive){
+  //       return stream
+  //     // }
+  //   })
+  // });
+  
+});
+
+async function run(): Promise<void> {
+  await mongoose.connect(<string>process.env.DBURL, 
+    (data) => {console.log("connected:", data)},
+  );
+
+  const socketBridge = new SocketBridge();
+
+  async function updateViewCount(pubKey, count) {
+    const viewer = await StreamModel.findOneAndUpdate({
+      publicKey: pubKey
+    }, {
+      viewerCount: count
+    });
+  }
+
+  socketBridge.onDisconnect((topic, count) => {
+    if(topic.substring(1,5) == "chat") {
+      updateViewCount(topic.substring(6), count);
+    }
+    console.log('topic ', topic, ' has ', count);
+  });
+  socketBridge.onConnect((topic, count) => {
+    if(topic.substring(1,5) == "chat") {
+      updateViewCount(topic.substring(6), count);
+    }
+    console.log('topic ', topic, ' has ', count);
+  });
+
+  socketBridge.start();
+
+  app.listen(port, (err?: string) => {
+    if(err){
+      return console.error(err);
+    }
+    return console.log(`API on ${port}`);
+  });
+}
+
+run().catch(err => console.log(err));
---
+++ .history/deso frontend/src/app/page/page.component_20211027232615.ts
@@ -0,0 +1,114 @@
+import { Component, HostListener, Input, OnInit, Output, EventEmitter } from "@angular/core";
+import { GlobalVarsService } from "../global-vars.service";
+import { Router, ActivatedRoute } from "@angular/router";
+import { BackendApiService } from "../backend-api.service";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "src/environments/environment";
+
+@Component({
+  selector: "app-page",
+  templateUrl: "./page.component.html",
+  styleUrls: ["./page.component.scss"],
+})
+export class PageComponent implements OnInit {
+  @Input() hideSidebar: string;
+  @Input() showPostButton = false;
+  @Input() inTutorial: boolean = false;
+
+  @Input() settings = true
+  @Input() creatorDashboard = true
+  @Input() backToSigma = true
+  @Input() channel = true
+  followedStreamersList
+  followedUsernameStreamersList = []
+  profilePicCounter = 0
+  @Input() streamerProfile = { PublicKeyBase58Check: "" } // might be problematic -- check the html
+  @Output() accountChanged = new EventEmitter()
+  mobile = false;
+  @Output() streamChanged = new EventEmitter()
+
+  @HostListener("window:resize") onResize() {
+    this.setMobileBasedOnViewport();
+  }
+
+  destory() {
+    console.log("destroy called")
+    this.followedStreamersList = []
+    this.followedUsernameStreamersList = []
+    this.profilePicCounter = 0
+  }
+
+  onAccountChange() {
+    this.destory()
+    this.accountChanged.emit()
+    this.followedStreamers()
+  }
+
+  changeStream(newStreamerUsername) {
+    this.destory()
+    // this.followedStreamers()
+    this.streamChanged.emit()
+    this.router.navigate([newStreamerUsername])
+  }
+
+  constructor(private http: HttpClient, public globalVars: GlobalVarsService, private router: Router, private backendApi: BackendApiService, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.destory()
+    this.setMobileBasedOnViewport();
+    this.followedStreamers()
+  }
+  
+  data
+
+  async followedStreamers() {
+    this.data = await this.backendApi.jwtGet(`${environment.apiURL}`, '/following', this.globalVars.loggedInUser.PublicKeyBase58Check).toPromise()
+    this.destory()
+    this.followedStreamersList = this.data
+
+
+    for (let i = 0; i < 10 && i < this.followedStreamersList.following.length; i++) {
+      this.data = await this.backendApi.GetSingleProfile(this.globalVars.localNode, this.followedStreamersList.following[i], "").toPromise()
+      this.followedUsernameStreamersList.push({ username: this.data.Profile.Username, publicKey: this.followedStreamersList.following[i] })
+      this.data = await this.backendApi.GetSingleProfilePicture(this.globalVars.localNode,
+        this.followedStreamersList.following[i]).toPromise()
+      this._readImageFileToProfilePicInput(this.data, i)
+
+    }
+  }
+
+  goToMessages() {
+    this.router.navigate(['/inbox'])
+  }
+
+  redirectToHomePage() {
+    this.router.navigate(['/'])
+  }
+
+  goToNotification() {
+    this.router.navigate(['/user', this.globalVars.RouteNames.NOTIFICATIONS])
+  }
+
+
+  setMobileBasedOnViewport() {
+    this.mobile = this.globalVars.isMobile();
+  }
+
+  _readImageFileToProfilePicInput(file: Blob | File, i) {
+    const reader = new FileReader();
+    reader.readAsBinaryString(file);
+    reader.onload = (event: any) => {
+      const base64Image = btoa(event.target.result);
+      try {
+
+        this.followedUsernameStreamersList[i].profilePicture = `data:${file.type};base64,${base64Image}`;
+        this.profilePicCounter++
+      } catch (err) {
+
+      }
+
+
+    };
+  }
+
+}
